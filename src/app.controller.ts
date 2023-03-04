fc5ccdaf48415737667ea416f877300c380a9b4b
TrocyukS96 <trotzuk.stanislav@gmail.com>
2023-03-05 01:31:25 +0300

    added config to connect to postgre BD & checked connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller('/api')
 export  class AppController{
+  constructor(private appService:AppService) {
+  }
 
   @Get('/users')
   getUsers(){
-    return [{id:1,name:'First user'}]
+    return this.appService.getUsers()
   }
 }
+
+//команды для установки зависимостей через seqilize для postgre
+//1) npm install --save @nestjs/sequelize sequelize sequelize-typescript
+//2) npm install --save pg pg-hstore
+//3) npm install --save-dev @types/sequelize
+//полезные ссылки на эту тему: https://sequelize.org/docs/v6/getting-started/
+//полезные ссылки на эту тему: https://docs.nestjs.com/techniques/database#sequelize-integration
