adc759b5ffdc61a98864865ff8a3eeac1ccce441
TrocyukS96 <trotzuk.stanislav@gmail.com>
2023-03-11 16:55:55 +0300

    added authGuard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,53 +2,53 @@ import { Body, HttpException, HttpStatus, Injectable, Post, UnauthorizedExceptio
 import { CreateUserDto } from "../users/dto/create-user-dto";
 import { UsersService } from "../users/users.service";
 import { JwtService } from "@nestjs/jwt";
-import * as bcrypt from 'bcryptjs'
+import * as bcrypt from "bcryptjs";
 import { User } from "../users/users.model";
 
 @Injectable()
 export class AuthService {
 
-  constructor(private userService:UsersService,
-              private jwtService:JwtService) {
+  constructor(private userService: UsersService,
+              private jwtService: JwtService) {
   }
 
- async login(userDto:CreateUserDto){
-    const user = this.validateUser(userDto)
-   return user;
+  async login(userDto: CreateUserDto) {
+    const user =await this.validateUser(userDto);
+    return this.generateToken(user);
   }
 
- async registration(userDto:CreateUserDto){
+  async registration(userDto: CreateUserDto) {
     //проверяем есть ли такой пользователь в БД
-    const candidate = await this.userService.getUserByEmail(userDto.email)
-   if(candidate){
-     throw new HttpException('Пользователь с таким email существует',HttpStatus.BAD_REQUEST)
-   }
-   const hashPassword = await bcrypt.hash(userDto.password,5);
-   //сразу создаем пользователя, но пороль перезаписываем на хэшированный
-   const user = await this.userService.createUser({...userDto,password:hashPassword})
-   return this.generateToken(user)
+    const candidate = await this.userService.getUserByEmail(userDto.email);
+    if (candidate) {
+      throw new HttpException("Пользователь с таким email существует", HttpStatus.BAD_REQUEST);
+    }
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
+    //сразу создаем пользователя, но пороль перезаписываем на хэшированный
+    const user = await this.userService.createUser({ ...userDto, password: hashPassword });
+    return this.generateToken(user);
   }
 
-  private  async generateToken(user:User){
+  private async generateToken(user: User) {
     //создадим обьект, который мы будем добавлять внутрь токена
     const payload = {
-      email:user.email,
-      id:user.id,
-      roles:user.roles
-    }
+      email: user.email,
+      id: user.id,
+      roles: user.roles
+    };
     //возвращаем сгенерированный токен с помощью сервиса jwtService
     return {
-      token:this.jwtService.sign(payload)
-    }
+      token: this.jwtService.sign(payload)
+    };
   }
 
-  private async validateUser(userDto:CreateUserDto){
-    const user = await this.userService.getUserByEmail(userDto.email)
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
     //делаем проверку:совпадает ли пороль который пришел с клиента с поролем из БД
-    const passwordEquals = await bcrypt.compare(userDto.password,user.password)
-    if(user && passwordEquals){
-      return user
+    const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+    if (user && passwordEquals) {
+      return user;
     }
-    throw new UnauthorizedException({message:'Неккоректный email или пороль'})
+    throw new UnauthorizedException({ message: "Неккоректный email или пороль" });
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user-dto";
 import { UsersService } from "./users.service";
 import {
@@ -7,6 +7,7 @@ import {
   ApiTags
 } from "@nestjs/swagger";
 import { User } from "./users.model";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 
 @ApiTags("Пользователи")
 @Controller("users")
@@ -23,6 +24,7 @@ export class UsersController {
 
   @ApiOperation({ summary: "Получение пользователей" })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.userService.getAllUsers();
